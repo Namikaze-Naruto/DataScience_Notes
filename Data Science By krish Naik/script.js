f6a84938a5f4f217031f784a841ad4bf6439c150
Prem Kumar <kumarprembgp2007@gmail.com>
2025-02-23 13:36:37 +0530

    Create script.js

---
+++ Data Science By krish Naik/script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll('nav ul li a');
+    links.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+            targetElement.scrollIntoView({ behavior: 'smooth' });
+        });
+    });
+});
